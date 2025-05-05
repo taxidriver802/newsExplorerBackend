da18154b3002e69e85a65fdab9256f29e35527ed
jason cox <JAcox12@icloud.com>
2025-05-05 01:14:51 -0500

    removed comments

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,14 +2,14 @@ const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = require("../utils/config");
 
 const authMiddleware = (req, res, next) => {
-  const token = req.headers.authorization?.split(" ")[1]; // Extract token from Authorization header
+  const token = req.headers.authorization?.split(" ")[1]; 
   if (!token) {
     return res.status(401).json({ message: "Authorization required" });
   }
 
   try {
-    const payload = jwt.verify(token, JWT_SECRET); // Verify token
-    req.user = payload; // Attach user payload to request
+    const payload = jwt.verify(token, JWT_SECRET); 
+    req.user = payload; 
     next();
   } catch (err) {
     res.status(401).json({ message: "Invalid or expired token" });
